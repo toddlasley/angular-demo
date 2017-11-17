023d65eccb8301f713ef44e43a11e4efad818be8
Todd <todd.r.lasley@gmail.com>
2017-11-17 16:45:56 -0500

    adding new pokemon-detail component

--- pokemon-app/src/app/app.module.ts
+++ pokemon-app/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { PokemonService } from '../services/pokemon.service';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonDetailComponent
   ],
   imports: [
     BrowserModule,
