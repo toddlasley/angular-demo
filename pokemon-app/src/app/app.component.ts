43f647f89c134b018e2ccdbb5b8ad878ad579a47
Todd <todd.r.lasley@gmail.com>
2017-11-17 17:28:01 -0500

    animations are now being handeld at the pokemon-detail component level

--- pokemon-app/src/app/app.component.ts
+++ pokemon-app/src/app/app.component.ts
@@ -1,39 +1,22 @@
 import { Component } from '@angular/core';
 import { Pokemon } from '../models/pokemon';
 import { PokemonService } from '../services/pokemon.service';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  animations: [trigger(
-    'openClose',
-    [
-      state('closed', style({height: '0px', display: 'none'})),
-      state('opened', style({height: '*'})),
-      transition( 'closed <=> opened', [animate(500, style({height: '50px'})), animate(500)])
-    ]
-  )]
+  styleUrls: ['./app.component.css']  
 })
 export class AppComponent {
   title = 'Pokemon App';
   pokemon: Pokemon[];
-  stateExpression: string;
 
   constructor(private pokemonService: PokemonService){}
 
   ngOnInit() {
     this.pokemonService.getPokemon()
       .subscribe(result => this.pokemon = result);
-  }
-
-  toggleOpenClose() {
-    if (this.stateExpression == 'closed') {
-      this.stateExpression = 'opened';
-    } else {
-      this.stateExpression = 'closed';
-    }
-  }
+  }  
 }
--- pokemon-app/src/app/pokemon-detail/pokemon-detail.component.ts
+++ pokemon-app/src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from '../../models/pokemon';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-pokemon-detail',
   templateUrl: './pokemon-detail.component.html',
-  styleUrls: ['./pokemon-detail.component.css']
+  styleUrls: ['./pokemon-detail.component.css'],
+  animations: [trigger(
+    'openClose',
+    [
+      state('closed', style({height: '0px'})),
+      state('opened', style({height: '*'})),
+      transition( 'closed <=> opened', [animate(500, style({height: '50px'})), animate(500)]),
+    ]
+  )]
 })
-export class PokemonDetailComponent implements OnInit {
+export class PokemonDetailComponent {
+  stateExpression: string;
+  @Input() pokemon: Pokemon;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    this.stateExpression = 'closed'
   }
 
+  toggleOpenClose() {
+    if (this.stateExpression == 'closed') {
+      this.stateExpression = 'opened';
+    } else {
+      this.stateExpression = 'closed';
+    }
+  }
 }
